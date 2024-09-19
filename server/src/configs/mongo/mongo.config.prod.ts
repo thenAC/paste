ee24b834de155f2bf99bddf70bcbc89687d603da
bLue <tbdblue@gmail.com>
2024-09-19 18:23:59 +0800

    fix(server): mongo prod issue

--- server/src/configs/mongo/mongo.config.prod.ts
+++ server/src/configs/mongo/mongo.config.prod.ts
@@ -3,7 +3,7 @@ import MongoConfig from './mongo.config';
 
 @Config(MongoConfig, { when: 'production', override: true })
 export default class MongoProdConfig extends MongoConfig {
-  public static readonly url = `mongodb://${process.env.MONGO_USER}:${encodeURIComponent(process.env.MONGO_PASS)}@${
+  public readonly url = `mongodb://${process.env.MONGO_USER}:${encodeURIComponent(process.env.MONGO_PASS)}@${
     process.env.MONGO_HOST || '127.0.0.1'
   }:${process.env.MONGO_PORT || 27017}/${process.env.MONGO_DB || 'thenac_paste'}`;
 }
